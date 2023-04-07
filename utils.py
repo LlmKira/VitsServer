a0a392f1755bdc9c3c7d441b7a0bf696f95e0224
sudoskys <coldlando@hotmail.com>
2023-04-08 02:24:37 +0800

    fix

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-import logging
 import os
 from json import loads
 
@@ -40,33 +39,32 @@ class HParams(object):
         return self.__dict__.__repr__()
 
 
-def load_checkpoint(checkpoint_path, model):
-    def load_checkpoint(checkpoint_path, model, optimizer=None):
-        assert os.path.isfile(checkpoint_path)
-        checkpoint_dict = load(checkpoint_path, map_location='cpu')
-        iteration = checkpoint_dict['iteration']
-        learning_rate = checkpoint_dict['learning_rate']
-        if optimizer is not None:
-            optimizer.load_state_dict(checkpoint_dict['optimizer'])
-        saved_state_dict = checkpoint_dict['model']
-        if hasattr(model, 'module'):
-            state_dict = model.module.state_dict()
-        else:
-            state_dict = model.state_dict()
-        new_state_dict = {}
-        for k, v in state_dict.items():
-            try:
-                new_state_dict[k] = saved_state_dict[k]
-            except:
-                logger.info("%s is not in the checkpoint" % k)
-                new_state_dict[k] = v
-        if hasattr(model, 'module'):
-            model.module.load_state_dict(new_state_dict)
-        else:
-            model.load_state_dict(new_state_dict)
-        logger.info("Loaded checkpoint '{}' (iteration {})".format(
-            checkpoint_path, iteration))
-        return model, optimizer, learning_rate, iteration
+def load_checkpoint(checkpoint_path, model, optimizer=None):
+    assert os.path.isfile(checkpoint_path)
+    checkpoint_dict = load(checkpoint_path, map_location='cpu')
+    iteration = checkpoint_dict['iteration']
+    learning_rate = checkpoint_dict['learning_rate']
+    if optimizer is not None:
+        optimizer.load_state_dict(checkpoint_dict['optimizer'])
+    saved_state_dict = checkpoint_dict['model']
+    if hasattr(model, 'module'):
+        state_dict = model.module.state_dict()
+    else:
+        state_dict = model.state_dict()
+    new_state_dict = {}
+    for k, v in state_dict.items():
+        try:
+            new_state_dict[k] = saved_state_dict[k]
+        except:
+            logger.info("%s is not in the checkpoint" % k)
+            new_state_dict[k] = v
+    if hasattr(model, 'module'):
+        model.module.load_state_dict(new_state_dict)
+    else:
+        model.load_state_dict(new_state_dict)
+    logger.info("Loaded checkpoint '{}' (iteration {})".format(
+        checkpoint_path, iteration))
+    return model, optimizer, learning_rate, iteration
 
 
 def get_hparams_from_file(config_path):
